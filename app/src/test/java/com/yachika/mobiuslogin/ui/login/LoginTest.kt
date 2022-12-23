8bb2ebe88be1422a707edcc76ab24bda33c83664
Yachika <nikkijoshi1997@gmail.com>
2022-12-23 13:56:07 +0530

    Add Mobius & RXJava dependencies with login test cases

---
+++ app/src/test/java/com/yachika/mobiuslogin/ui/login/LoginTest.kt
@@ -0,0 +1,33 @@
+package com.yachika.mobiuslogin.ui.login
+
+import org.junit.Test
+
+class LoginTest {
+
+    @Test
+    fun `empty username returns false`() {
+
+    }
+
+    @Test
+    fun `empty password returns false`() {
+
+    }
+
+    @Test
+    fun `password contain less than 8 digits returns false`() {
+
+    }
+
+
+    @Test
+    fun `when login button clicks with correct credentials, user should redirect to dashboard`() {
+
+    }
+
+    @Test
+    fun `when login button clicks with incorrect credentials, invalid password message should display`() {
+
+    }
+
+}
